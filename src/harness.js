e7e1d4dfddf1ca4e2e983349da0f90958fe90590
Joe McCourt <joe@aviary.com>
2012-12-02 00:48:57 -0500

    Lots of fleshing out.  Now interactive graphs.  Started using namespace JFWL.

--- src/harness.js
+++ src/harness.js
@@ -1,17 +1,75 @@
 window.onload = function(){
-	var canvas = document.getElementById("demoCanvas");
+	// var line1 = [-0.1,-0.5,0.9,0.9];
+	// var line2 = [0.7,0,-0.7,0.6];
 
-	var w = canvas.width;
-	var h = canvas.height;
+	// var lines = [line1,line2];
+	// drawLine(canvas,lines[0]);
+	// drawLine(canvas,lines[1]);
 
-	var context = canvas.getContext("2d");
+
+	//var lines = [];
+	//var numLines = 1000;
+	//var i;
+
+	// for(i = 0; i < numLines; i++){
+	// 	lines[i] = [];
+
+	// 	// uniform random
+	// 	// lines[i][0] = (Math.random()-0.5)*2;
+	// 	// lines[i][1] = (Math.random()-0.5)*2;
+	// 	// lines[i][2] = (Math.random()-0.5)*2;
+	// 	// lines[i][3] = (Math.random()-0.5)*2;
+
+	// 	//Const length
+	// 	lines[i][0] = (Math.random()-0.5)*2;
+	// 	lines[i][1] = (Math.random()-0.5)*2;
+
+	// 	var length = 0.15;
+	// 	var angle = Math.random() * Math.PI * 2;
+
+	// 	lines[i][2] = lines[i][0] + length*Math.cos(angle);
+	// 	lines[i][3] = lines[i][1] + length*Math.sin(angle);
+	// }
+
+	JFWL.canvas = document.getElementById("demoCanvas");
+	JFWL.graph = genGraph();
+
+	JFWL.initEvents();
+
+	//Main loop
+	window.setInterval(function(){
+		var start = JFWL.clockTime;
+		// while(JFWL.clockTime - start < JFWL.refreshRate / 1000){
+		// 	JFWL.iterations++;
+		// };
+
+		JFWL.reDraw();
+		//JFWL.display();
+	},0);
+};
+
+//Global vars
+var JFWL = {}; //Joe's Fun With Lines
+
+JFWL.startTime = (new Date()).getTime();
+JFWL.clockTime = 0;
+JFWL.refreshRate = 30;
+JFWL.iterations = 0;
+JFWL.mouse = "up";
+JFWL.hoverNode  = -1;
+JFWL.dragNode   = -1;
+JFWL.dragNodeMoved = false;
+JFWL.clickOffset = {x:0,y:0};
+
+JFWL.reDraw = function(){
+	var context = JFWL.canvas.getContext("2d");
 	var ctx = context; //alias
 
-	//var oldCanvasData = context.getImageData(0, 0, canvas.width, canvas.height);
-	var newCanvasData = context.createImageData(w, h); // blank
+	var w = JFWL.canvas.width;
+	var h = JFWL.canvas.height;
 	
+	var newCanvasData = context.createImageData(w, h); // blank
 	var dst = newCanvasData.data;
-	// var src = oldCanvasData.data;
 	
 	var n;
 	for(n = 0; n < w*h; n++){
@@ -22,44 +80,19 @@ window.onload = function(){
 	}
 
 	context.putImageData(newCanvasData, 0, 0);
-	
-	// var line1 = [-0.1,-0.5,0.9,0.9];
-	// var line2 = [0.7,0,-0.7,0.6];
-
-	// var lines = [line1,line2];
-	// drawLine(canvas,lines[0]);
-	// drawLine(canvas,lines[1]);
 
 	var intersectsColor = 'f00';
 	var multiIntersectsColor = '00f';
 	var defaultColor = '0f0';
+	var intersects = [];
 
-	var lines = [];
-	var intersects = []
-	var numLines = 1000;
-	var i;
+	var numLines = JFWL.graph.lines.length;
+	var lines = genLinesArray();
+	
+	markIntersections(intersects);
 
-	for(i = 0; i < numLines; i++){
-		lines[i] = [];
-
-		// uniform random
-		// lines[i][0] = (Math.random()-0.5)*2;
-		// lines[i][1] = (Math.random()-0.5)*2;
-		// lines[i][2] = (Math.random()-0.5)*2;
-		// lines[i][3] = (Math.random()-0.5)*2;
-
-		//Const length
-		lines[i][0] = (Math.random()-0.5)*2;
-		lines[i][1] = (Math.random()-0.5)*2;
-
-		var length = 0.08;
-		var angle = Math.random() * Math.PI * 2;
-
-		lines[i][2] = lines[i][0] + length*Math.cos(angle);
-		lines[i][3] = lines[i][1] + length*Math.sin(angle);
-	}
-
-	markLineIntersections(lines,intersects);
+	//console.log(graph,lines);
+	//countLineIntersections(lines,intersects);
 
 	var color;
 	for(i = 0; i < numLines; i++){
@@ -70,6 +103,92 @@ window.onload = function(){
 		}else{
 			color = defaultColor;
 		}
-		drawLine(canvas,lines[i],color);
+		drawLine(lines[i],color);
 	}
+
+	drawNodes();
 };
+
+
+//Events
+JFWL.initEvents = function(){
+	$(document).mouseup(function (e) {
+		JFWL.mouse = "up";
+		if(JFWL.dragNodeMoved){
+			JFWL.dragNode = -1;
+		}
+	});
+
+	$(document).mousedown(function (e) {
+		JFWL.mouse = "down";
+
+		if(JFWL.dragNode < 0 && JFWL.hoverNode >= 0){
+			JFWL.dragNode = JFWL.hoverNode;
+			JFWL.hoverNode = -1;
+
+			var offset = $("#demoCanvas").offset();
+			var x = e.pageX - offset.left;
+			var y = e.pageY - offset.top;
+
+			var w = JFWL.canvas.width;
+			var h = JFWL.canvas.height;
+
+			//Convert to my coord system
+			x = 2*x/w-1;
+			y = 2*y/h-1;
+
+			JFWL.clickOffset.x = x - JFWL.graph.nodes[JFWL.dragNode].x;
+			JFWL.clickOffset.y = y - JFWL.graph.nodes[JFWL.dragNode].y;
+		}else{
+			JFWL.dragNode = -1;
+		}
+
+		JFWL.dragNodeMoved = false;
+	});
+
+	$(document).mousemove(function (e) {
+
+		var offset = $("#demoCanvas").offset();
+		var x = e.pageX - offset.left;
+		var y = e.pageY - offset.top;
+
+		var w = JFWL.canvas.width;
+		var h = JFWL.canvas.height;
+
+		//Convert to my coord system
+		x = 2*x/w-1;
+		y = 2*y/h-1;
+
+		if(JFWL.dragNode < 0){
+			hoverAt(x,y);
+		}else{
+
+			var dist = Math.sqrt(Math.pow(JFWL.graph.nodes[JFWL.dragNode].x - (x - JFWL.clickOffset.x),2)+Math.pow(JFWL.graph.nodes[JFWL.dragNode].y - (y - JFWL.clickOffset.y),2));
+
+			if(JFWL.dragNodeMoved || dist > 0.03){
+				JFWL.dragNodeMoved = true;
+				JFWL.moveNode(JFWL.dragNode, x - JFWL.clickOffset.x, y - JFWL.clickOffset.y);
+			}
+		}
+
+		//console.log(2*x/w-1,2*y/h-1);
+	});
+};
+
+function hoverAt(x,y){
+	var graph = JFWL.graph;
+
+	var i, dist;
+	var minDist = 10; var minIndex = -1;
+	for(i = 0; i < graph.nodes.length; i++){
+		dist = Math.sqrt(Math.pow(graph.nodes[i].x - x, 2) + Math.pow(graph.nodes[i].y - y, 2));
+		if(dist < minDist && dist < 0.03){
+			minIndex = i;
+			minDist  = dist;
+		}
+	}
+
+	JFWL.hoverNode = minIndex;
+};
+
+
--- src/lines.js
+++ src/lines.js
@@ -1,10 +1,10 @@
-function drawLine(canvas,line,color){
+function drawLine(line,color){
 	if(typeof color === "undefined"){color = '000';}
 
-	var ctx = canvas.getContext("2d");
+	var ctx = JFWL.canvas.getContext("2d");
 
-	var w = canvas.width;
-	var h = canvas.height;
+	var w = JFWL.canvas.width;
+	var h = JFWL.canvas.height;
 
 	var x1 = (line[0]+1)*w/2;
 	var y1 = (line[1]+1)*h/2;
@@ -18,7 +18,52 @@ function drawLine(canvas,line,color){
     ctx.stroke();
 }
 
-function markLineIntersections(lines,intersects){
+JFWL.moveNode = function(node,x,y){
+
+	//Clamp
+	x = Math.max(-1,Math.min(1,x));
+	y = Math.max(-1,Math.min(1,y));
+
+	JFWL.graph.nodes[node].x = x;
+	JFWL.graph.nodes[node].y = y;
+}
+
+function drawNode(node,color){
+	if(typeof color === "undefined"){color = '000';}
+
+	var ctx = JFWL.canvas.getContext("2d");
+
+	var w = JFWL.canvas.width;
+	var h = JFWL.canvas.height;
+
+	var x = (node.x+1)*w/2;
+	var y = (node.y+1)*h/2;
+
+	ctx.beginPath();
+	ctx.arc(x, y, 4, 0, 2 * Math.PI, false);
+	ctx.fillStyle = color;
+	ctx.fill();
+	ctx.lineWidth = 0;
+	ctx.strokeStyle = color;
+    ctx.stroke();
+}
+
+function drawNodes(color){
+	var graph = JFWL.graph;
+	var n = graph.nodes.length;
+	var i;
+	for(i = 0; i < n; i++){
+		if(i == JFWL.hoverNode){
+			drawNode(graph.nodes[i],'00f');
+		}else if(i == JFWL.dragNode){
+			drawNode(graph.nodes[i],'f00');
+		}else{
+			drawNode(graph.nodes[i],color);
+		}
+	}
+}
+
+function countLineIntersections(lines,intersects){
 
 	//Brute force
 	var n = lines.length;
@@ -36,20 +81,66 @@ function markLineIntersections(lines,intersects){
 			}
 		}
 	}
-
-	// for(i = 0; i < n; i++){
-	// 	if(!intersects[i]){
-	// 		for(j = 0; j < n; j++){
-	// 			if(lineIntersectsLine(lines[i],lines[j])){
-	// 				intersects[i] = true;
-	// 				intersects[j] = true;
-	// 				break;
-	// 			}
-	// 		}
-
-	// 	}
-	// }
 }
+
+
+function markIntersections(intersects){
+
+	//Brute force
+	var graph = JFWL.graph;
+	var n = graph.lines.length;
+	var lines = genLinesArray(graph);
+
+	//Set intersects to all false initially
+	var i,j;
+	var lineI,lineJ;
+	for(i = 0; i < n; i++){
+		intersects[i] = false;
+	}
+
+	lineI = [];
+	lineJ = [];
+	for(i = 0; i < n; i++){
+		if(!intersects[i]){
+			for(j = 0; j < n; j++){
+				if(graph.lines[i][0] == graph.lines[j][0] || graph.lines[i][1] == graph.lines[j][0] || graph.lines[i][0] == graph.lines[j][1] || graph.lines[i][1] == graph.lines[j][1]){
+					//Share same node, count as not interesecting
+				}else if(lineIntersectsLine(lines[i],lines[j])){
+					intersects[i] = true;
+					intersects[j] = true;
+					break;
+				}
+			}
+
+		}
+	}
+}
+
+
+// function markLineIntersections(lines,intersects){
+
+// 	//Brute force
+// 	var n = lines.length;
+
+// 	//Set intersects to all false initially
+// 	var i,j;
+// 	for(i = 0; i < n; i++){
+// 		intersects[i] = false;
+// 	}
+
+// 	for(i = 0; i < n; i++){
+// 		if(!intersects[i]){
+// 			for(j = 0; j < n; j++){
+// 				if(lineIntersectsLine(lines[i],lines[j])){
+// 					intersects[i] = true;
+// 					intersects[j] = true;
+// 					break;
+// 				}
+// 			}
+
+// 		}
+// 	}
+// }
 
 function lineIntersectsLine(line1,line2){
 	
@@ -66,7 +157,12 @@ function lineIntersectsLine(line1,line2){
 	
 	var denom = (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4);
 
-	if(denom == 0){return false;} //Lines are parallel
+	if(denom == 0){
+
+		//Lines are parallel
+		//TODO: test parallel lines intersection
+		return false;
+	}
 
 	var px = ((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/denom;
 	var py = ((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/denom;
@@ -82,4 +178,64 @@ function lineIntersectsLine(line1,line2){
 	if(py > y3 && py > y4){return false;}
 
 	return true;
+}
+
+
+function genLinesArray(){
+	var graph = JFWL.graph;
+
+	var lines = [];
+	var numLines = graph.lines.length;
+
+	var i;
+	for(i = 0; i < numLines; i++){
+		lines[i] = [];
+		lines[i][0] = graph.nodes[graph.lines[i][0]].x;
+		lines[i][1] = graph.nodes[graph.lines[i][0]].y;
+		lines[i][2] = graph.nodes[graph.lines[i][1]].x;
+		lines[i][3] = graph.nodes[graph.lines[i][1]].y;
+	}
+
+	return lines;
+}
+
+function genGraph(){
+	// var graph = {
+	// 	nodes:[
+	// 		{x:0,y:0},
+	// 		{x:1,y:1},
+	// 		{x:0.5,y:-0.5},
+	// 		{x:0.1,y:0.5}
+	// 		],
+	// 	lines:[
+	// 		[0,1],
+	// 		[1,2],
+	// 		[2,3]
+	// 	]
+	// };
+
+	JFWL.graph = {nodes:[],lines:[]}
+	var graph = JFWL.graph;
+
+	var numNodes = 10;
+	var numLines = 15;
+	var i;
+	for(i = 0; i < numNodes; i++){
+		graph.nodes[i] = {};
+		graph.nodes[i].x = (Math.random()-0.5)*2;
+		graph.nodes[i].y = (Math.random()-0.5)*2;
+	}
+
+	var nodeI,nodeJ;
+	for(i = 0; i < numLines; i++){
+		nodeI = Math.floor(Math.random()*numNodes);
+		nodeJ = nodeI;
+		while(nodeJ == nodeI){
+			nodeJ = Math.floor(Math.random()*numNodes);
+		}
+
+		graph.lines[i] = [nodeI,nodeJ];
+	}
+
+	return graph;
 }
